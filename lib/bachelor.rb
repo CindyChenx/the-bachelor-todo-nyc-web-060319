f22b7f98be4071c99ab09198e37035cd6df1c8b0
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-31 05:14:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winning_name = nil
+  data[season].each do |contest_hash|
+    if contest_hash["status"] == "Winner"
+      winning_name = contest_hash["name"].split[0]
+    end
+  end
+  return winning_name
 end
 
 def get_contestant_name(data, occupation)
